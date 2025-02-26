8bb115b3e2739c91314a005aab7b738956c165e8
SaiChara17 <saicharanpunna7@gmail.com>
2025-02-26 22:12:41 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,33 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import express  from "express"
+import cors from 'cors'
+import { connectDB } from "./config/db.js"
+import userRouter from "./routes/userRoute.js"
+import foodRouter from "./routes/foodRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
 
-// Load environment variables from .env file
-dotenv.config();
+// app config
+const app = express()
+const port = process.env.PORT || 4000;
 
-export const connectDB = async () => {
-  try {
-    // Use the MONGODB_URI environment variable
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("DB Connected");
-  } catch (error) {
-    console.error("DB Connection Error:", error);
-    process.exit(1); // Exit the process if the connection fails
-  }
-};
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// db connection
+connectDB()
+
+// api endpoints
+app.use("/api/user", userRouter)
+app.use("/api/food", foodRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/cart", cartRouter)
+app.use("/api/order",orderRouter)
+
+app.get("/", (req, res) => {
+    res.send("API Working")
+  });
+
+app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
